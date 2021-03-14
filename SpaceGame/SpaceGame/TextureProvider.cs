0cd636d894965d7b04250400f0536b95db0e12d8
Zintom <tzsinkoapps@gmail.com>
2021-03-14 00:19:27 +0000

    Create TextureProvider.cs
    Loads static textures for the game.

---
+++ SpaceGame/SpaceGame/TextureProvider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame
+{
+    public class TextureProvider
+    {
+
+        public Texture2D Ship { get; private set; }
+
+        public Texture2D LargeAsteroid1 { get; private set; }
+        public Texture2D LargeAsteroid2 { get; private set; }
+
+        public Texture2D SmallAsteroid1 { get; private set; }
+        public Texture2D SmallAsteroid2 { get; private set; }
+
+        public TextureProvider(ContentManager content)
+        {
+            Ship = content.Load<Texture2D>("spaceship");
+
+            LargeAsteroid1 = content.Load<Texture2D>("large_asteroid_1");
+            LargeAsteroid2 = content.Load<Texture2D>("large_asteroid_2");
+
+            SmallAsteroid1 = content.Load<Texture2D>("small_asteroid_1");
+            SmallAsteroid2 = content.Load<Texture2D>("small_asteroid_2");
+        }
+
+    }
+}
