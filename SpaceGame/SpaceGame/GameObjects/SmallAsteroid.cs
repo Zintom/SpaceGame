8e481cf032364c7ffd20f449fc96f3cfffd7ea45
Zintom <tzsinkoapps@gmail.com>
2021-03-14 19:58:09 +0000

    Create SmallAsteroid.cs

---
+++ SpaceGame/SpaceGame/GameObjects/SmallAsteroid.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SpaceGame.Helpers;
+
+namespace SpaceGame.GameObjects
+{
+    public class SmallAsteroid : Asteroid
+    {
+        public override Point Size { get; } = new Point(24, 24);
+
+        protected override Texture2D _texture { get; init; }
+
+        public SmallAsteroid(Vector2 startPosition, IPlayingFieldManager playingFieldManager, TextureProvider textureProvider, Viewport viewport) : base(playingFieldManager, textureProvider, viewport, 0.1f)
+        {
+            Position = startPosition;
+
+            int textureChoice = MainGame.RNG.NextRandom();
+
+            _texture = textureChoice % 2 == 0 ? textureProvider.SmallAsteroid1 : textureProvider.SmallAsteroid2;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, new Rectangle(Position.ToPoint(), Size), Color.White);
+        }
+
+        public override void Hit()
+        {
+            _playingFieldManager.RemoveAsteroid(this);
+        }
+    }
+}
