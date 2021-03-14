a3e531c7b2ea3c016da974a3e3a1734bb0d66b23
Zintom <tzsinkoapps@gmail.com>
2021-03-14 19:58:26 +0000

    Create Extensions.cs
    Contains helper functions for random number generation.

---
+++ SpaceGame/SpaceGame/Helpers/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame.Helpers
+{
+    public static class Extensions
+    {
+
+        public static int NextRandom(this RandomNumberGenerator generator)
+        {
+            Span<byte> rndBytes = stackalloc byte[4];
+
+            generator.GetBytes(rndBytes);
+
+            return BitConverter.ToInt32(rndBytes);
+        }
+
+        public static int NextRandom(this RandomNumberGenerator generator, int min, int max)
+        {
+            Span<byte> rndBytes = stackalloc byte[4];
+
+            generator.GetBytes(rndBytes);
+
+            return Math.Max(BitConverter.ToInt32(rndBytes) % max, min);
+        }
+
+    }
+}
