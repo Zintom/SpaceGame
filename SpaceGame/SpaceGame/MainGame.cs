92808c1b5476e4fcdecb936860ebb79b159d8432
Zintom <tzsinkoapps@gmail.com>
2021-03-14 19:58:40 +0000

    Update MainGame.cs

--- SpaceGame/SpaceGame/MainGame.cs
+++ SpaceGame/SpaceGame/MainGame.cs
@@ -2,15 +2,21 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SpaceGame.GameObjects;
+using SpaceGame.Helpers;
+using System;
+using System.Security.Cryptography;
 
 #nullable disable
 namespace SpaceGame
 {
     public class MainGame : Game
     {
+        public static readonly RandomNumberGenerator RNG = RandomNumberGenerator.Create();
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private TickCounter frameCounter = new TickCounter();
         private TextureProvider _textureProvider;
 
         PlayingFieldManager _playingField;
@@ -27,8 +33,12 @@ namespace SpaceGame
 
         protected override void Initialize()
         {
+            //TargetElapsedTime = TimeSpan.FromMilliseconds(1000.0d / 120d);
+            IsFixedTimeStep = false;
+
             _graphics.PreferredBackBufferWidth = 640;
             _graphics.PreferredBackBufferHeight = 480;
+            _graphics.SynchronizeWithVerticalRetrace = false;
             _graphics.ApplyChanges();
 
             Blank = new Texture2D(_graphics.GraphicsDevice, 1, 1);
@@ -43,11 +53,11 @@ namespace SpaceGame
             _textureProvider = new TextureProvider(Content);
 
             _playingField = new PlayingFieldManager(_textureProvider, _graphics.GraphicsDevice.Viewport);
-            _player = new Ship(_textureProvider, _graphics.GraphicsDevice.Viewport);
+            _player = new Ship(_textureProvider, _graphics.GraphicsDevice.Viewport, _playingField);
 
             // Center the player in the screen.
-            _player.Position = new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2 - Ship.Size.X / 2, 
-                                          _graphics.GraphicsDevice.Viewport.Height / 2 - Ship.Size.Y / 2);
+            _player.Position = new Vector2(_graphics.GraphicsDevice.Viewport.Width / 2 - _player.Size.X / 2, 
+                                          _graphics.GraphicsDevice.Viewport.Height / 2 - _player.Size.Y / 2);
         }
 
         protected override void Update(GameTime gameTime)
@@ -65,9 +75,13 @@ namespace SpaceGame
         {
             GraphicsDevice.Clear(Color.Black);
 
+            frameCounter.Tick(gameTime);
+
             _spriteBatch.Begin();
             _playingField.Draw(_spriteBatch);
-            _player.Draw(gameTime, _spriteBatch);
+            _player.Draw(_spriteBatch);
+
+            _spriteBatch.DrawString(_textureProvider.GameFont, "FPS: " + frameCounter.TicksPerSecond, new Vector2(10, 10), Color.White);
 
             //_spriteBatch.Draw(Blank, new Rectangle(0, 0, 10, 10), Color.White);
             _spriteBatch.End();
