b16d31c1963e8eae0fd0916784ccbe96fb39d563
Zintom <tzsinkoapps@gmail.com>
2021-03-14 00:21:39 +0000

    Create CommonGameObject.cs
    Base class for the Ship and Asteroids.

---
+++ SpaceGame/SpaceGame/GameObjects/CommonGameObject.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame.GameObjects
+{
+    public class CommonGameObject
+    {
+
+        public Vector2 Position { get; set; } = Vector2.Zero;
+
+        protected readonly Viewport _viewport;
+
+        public CommonGameObject(Viewport viewport)
+        {
+            _viewport = viewport;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            KeepInViewPortBounds();
+        }
+
+        private void KeepInViewPortBounds()
+        {
+            if (Position.X < 0)
+            {
+                Position = new Vector2(_viewport.Width, Position.Y);
+            }
+            if (Position.Y < 0)
+            {
+                Position = new Vector2(Position.X, _viewport.Height);
+            }
+            if (Position.X > _viewport.Width)
+            {
+                Position = new Vector2(0, Position.Y);
+            }
+            if (Position.Y > _viewport.Height)
+            {
+                Position = new Vector2(Position.X, 0);
+            }
+        }
+
+    }
+}
